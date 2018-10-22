ec097dc355faaad23956e3ba7f61baec5ec94117
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    Add VOLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5301,6 +5301,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to show VoLTE icon or not
+         * @hide
+         */
+        public static final String SHOW_VOLTE_ICON = "volte_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
