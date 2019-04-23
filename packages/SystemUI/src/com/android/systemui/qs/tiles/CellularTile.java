013a0e8b6b5c3f0c5973bf23df61726875e30db6
eray orçunus <erayorcunus@gmail.com>
2019-04-23 16:46:35 +0300

    SystemUI: Enable and fix QS detail view, adapt layout to Pie
    * Re-enabling dual-target(detail view) was easy as pie, but layout was from Oreo
      and DND tile was broken.
    
    * I've applied QS Customize layout to details view to match it with Pie look.
    
    * DND showDetail function was overriden to show new EnableZenModeDialog.
      New dialog's implementation were taken from old detail view, so I've disabled
      the new one, added the only new thing (priority-but-alarm-disabled warning text)
      to old one and enabled it.
    
    * DND tile will still respect "Ask duration every time" setting and ask duration to
      the user, but this time with detail view.
    
    In addition:
    ---------
    
    SystemUI: Enable dualTarget on CellularTile
    
    * To show DataUsageDetailView
    
    Change-Id: If09b4ac51bb6df01b8ea9f59bccf123de65da8cf
    (Owner: Chengzhi Hou <hcz017@gmail.com> )
    
    ---------
    
    SystemUI: CellularTile: Don't call showDetail() when device is locked
    
    * We recently enabled dualTarget for CellularTile which allowed us
      to toggle mobile data without unlocking device. Moving showDetail
      call to postQSRunnableDismissingKeyguard when the device is locked
      fixes this issue.
    
    Change-Id: I16dc766166a33a309a2fc945e81fa15b5d194196
    (Owner: Łukasz Patron <priv.luk@gmail.com> )
    
    ---------
    
    SystemUI: Fix Data Usage tile to match Settings
    
    Change-Id: Ieff3504baf3d13c77d06fcde87d5ac56d2586194
    (Owner: Paul Keith <javelinanddart@gmail.com>)
    
    ---------
    
    Author: eray orçunus <erayorcunus@gmail.com>
    Date:   Fri Apr 26 00:15:30 2019 +0300
    
    SystemUI: Fix several layout bugs
    
    * QSDetail and QSCustomize panels' clippers starts after the black
      header, but the ripple animation coords were wrong.
    
    * QSDetail and QSCustomize use a transparent view to let QS header
      be seen. Header's height changes with orientation, but QSDetail
      and QSCustomize do nothing about it.
      As a result, there were gaps or blocked views.
    
    Change-Id: I5cefad14b519be99adcf5c913e96385d45b8fb17
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    
    ---------
    
    Change-Id: I5ecb419fbf699be33494d0a9cc215914d90004bd
    [jaysonedson: Changed toolbar to transparent to match android11]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Change-Id: I1704162f6c16762a3e4c5c237f28b784ff7ffd97

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -65,6 +65,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
 
     private final CellSignalCallback mSignalCallback = new CellSignalCallback();
     private final ActivityStarter mActivityStarter;
+    private final KeyguardStateController mKeyguard;
 
     @Inject
     public CellularTile(QSHost host, KeyguardStateController keyguardStateController,
@@ -73,6 +74,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
         super(host);
         mController = networkController;
         mActivityStarter = activityStarter;
+        mKeyguard = keyguardStateController;
         mDataController = mController.getMobileDataController();
         mDetailAdapter = new CellularDetailAdapter();
         mController.observe(getLifecycle(), mSignalCallback);
@@ -148,7 +150,16 @@ public class CellularTile extends QSTileImpl<SignalState> {
 
     @Override
     protected void handleSecondaryClick() {
+        if (getState().state == Tile.STATE_UNAVAILABLE) {
+            return;
+        }
         if (mDataController.isMobileDataSupported()) {
+           if (mKeyguard.isMethodSecure() && mKeyguard.isShowing()) {
+                mActivityStarter.postQSRunnableDismissingKeyguard(() -> {
+                    showDetail(true);
+                });
+                return;
+            }
             showDetail(true);
         } else {
             mActivityStarter
@@ -169,6 +180,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
         }
 
         final Resources r = mContext.getResources();
+        state.dualTarget = true;
         state.label = r.getString(R.string.mobile_data);
         boolean mobileDataEnabled = mDataController.isMobileDataSupported()
                 && mDataController.isMobileDataEnabled();
