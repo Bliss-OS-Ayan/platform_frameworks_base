0c91f7a7d17893b5ca4815c2a917b6f98f9fc552
Kshitij Gupta <kshitijgm@gmail.com>
2019-04-27 17:33:44 +0530

    base: Introduce RGB accenter [1/2]
    SDB: Adapt for All Normal AOSP Base. Not everyone is comfortable with Dart & Gradle ;)
    AshutoshSundresh: Fix SystemSettingsValidators with ANY_INTEGER_VALIDATOR
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I5f3fcf5e8c1db47117662a22d1c79852275c33c9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5619,6 +5619,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_USE_NEW_TINT = "qs_panel_bg_use_new_tint";
 
         /**
+         * Accent Color
+         * @hide
+         */
+        public static final String ACCENT_COLOR = "accent_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5764,6 +5770,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_STYLE);
             PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
             PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
+            PRIVATE_SETTINGS.add(ACCENT_COLOR);
         }
 
         /**
