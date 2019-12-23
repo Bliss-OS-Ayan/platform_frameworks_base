d489a95b0ada2fb095b1a019f79321d69cc91c13
ezio84 <brabus84@gmail.com>
2019-12-23 10:34:01 +0100

    Statusbar clock: make it play better with expanded QS date
    Change-Id: I7055e4e682abb29c4cea08a4c8e93758c20365ab
    
    Statusbar clock date: behave like on stock when showing QS panel
    
    if the user shows collapsed qs panel, let's drop the date from statusbar
    clock and show the extended date view in the quick statusbar header,
    like on stock.
    
    This makes the user always able to have a complete date view even if
    he sets a custom date (e.g. the day number only) in statusbar clock
    
    Change-Id: Iac6e784b2015e9e411166fcdbaa072cc3afce20a
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -280,6 +280,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mDateView = findViewById(R.id.date);
         mSpace = findViewById(R.id.space);
         mDateView.setOnClickListener(this);
+        mClockView.setQsHeader();
 
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
