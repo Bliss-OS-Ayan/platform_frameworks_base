CherishOS/android_frameworks_base

Add MIUI volume style plugin [1/3]
@hungphan2001:Bring to R

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
@@ -76,6 +76,7 @@ public class VolumePluginManager extends BroadcastReceiver {
         "co.potatoproject.plugin.volume.compact",
         "co.potatoproject.plugin.volume.oreo",
         "co.potatoproject.plugin.volume.tiled",
+        "com.cherish.plugin.volume.miui",
     };
 
     private PluginPrefs mPluginPrefs;

Bliss-OS-Ayan/platform_frameworks_base

Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>
Change-Id: I73468d79ad4a512179fa70e7e2b8e3679ba50a63

packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
@@ -76,6 +76,7 @@ public class VolumePluginManager extends BroadcastReceiver {
         "co.potatoproject.plugin.volume.compact",
         "co.potatoproject.plugin.volume.oreo",
         "co.potatoproject.plugin.volume.tiled",
+        "co.potatoproject.plugin.volume.miui",
     };
 
     private PluginPrefs mPluginPrefs;